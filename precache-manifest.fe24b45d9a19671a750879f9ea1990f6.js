6c764643dda7f78f83c856fa1ca9ba2f2bc82479
SerGolub89 <k3210389gsi@gmail.com>
2019-11-20 14:36:36 +0200

    Updates

---
+++ precache-manifest.fe24b45d9a19671a750879f9ea1990f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969851e454f4642afffd4f52e2b74097",
+    "url": "/testTask/index.html"
+  },
+  {
+    "revision": "1f1485d2353c86194e06",
+    "url": "/testTask/static/css/2.074064f9.chunk.css"
+  },
+  {
+    "revision": "d4b6402c5b35339157e8",
+    "url": "/testTask/static/css/main.ecfb9755.chunk.css"
+  },
+  {
+    "revision": "1f1485d2353c86194e06",
+    "url": "/testTask/static/js/2.4b34bb5f.chunk.js"
+  },
+  {
+    "revision": "d4b6402c5b35339157e8",
+    "url": "/testTask/static/js/main.ac32a16e.chunk.js"
+  },
+  {
+    "revision": "af34c1a8d4bfaca4e423",
+    "url": "/testTask/static/js/runtime-main.624c3c41.js"
+  }
+]);
